71f0371e96d08fc7fc28297d84d3e8844bfdcc8e
jacklee2887 <jacklcl98@hotmail.com>
2018-08-07 17:42:51 +0800

    Completed User Authentication Features

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+	before_action :find_user, only: [:show, :update, :destroy]
+	
+	def new
+		@user = User.new
+	end
+
+	def create
+		user = User.new(user_params)
+		if user.save
+			flash[:success] = "User succesfully created!"
+			redirect_to '/login'
+		else
+			flash[:error] = "Invalid Email or Password"
+			redirect_to '/signup'
+		end
+	end
+
+	def show
+	end
+
+	private
+	def find_user
+		@user = User.find(params[:id])
+	end
+
+	def user_params
+ 	params.require(:user).permit(:email, :password, :password_confirmation)
+	end
+end
+
